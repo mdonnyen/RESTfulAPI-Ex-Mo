d0b24d35f203916dec3373991734b143e0b33edf
mdonnyen <mdonnyen@gmail.com>
2022-05-09 09:25:08 +0700

    Update Route createdUser (POST)

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,14 @@
 const Model = require("../models/model");
 
 module.exports = {
-  createUser: function(req, res) {
-    res.send("create a user");
+  createUser: async function(req, res) {
+    const newUser = await new Model(req.body) ;
+    try {
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }
   },
   getAllUser: function(req, res) {
     res.send("get all users");
